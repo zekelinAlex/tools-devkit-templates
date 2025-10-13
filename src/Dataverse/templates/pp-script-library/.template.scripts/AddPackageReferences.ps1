57ac4acf1278a03fc5d65cb8a145c543f766e49e
Alexander Zekelin <aleksander.zekelin@networg.com>
2025-10-13 09:31:54 +0200

    script library cdsproj update

--- src/Dataverse/templates/pp-script-library/.template.scripts/AddPackageReferences.ps1
+++ src/Dataverse/templates/pp-script-library/.template.scripts/AddPackageReferences.ps1
@@ -17,7 +17,14 @@ $projectFile = $projectFiles[0]
 $existingSolutionRef = $xml.SelectSingleNode("//PackageReference[@Include='TALXIS.SDK.BuildTargets.CDS.Solution']")
 $existingTypeScriptRef = $xml.SelectSingleNode("//PackageReference[@Include='TALXIS.SDK.BuildTargets.CDS.TypeScriptCommon']")
 
-if ($existingSolutionRef -and $existingTypeScriptRef) {
+$existingCoreAssemblies = $xml.SelectSingleNode("//PackageReference[@Include='Microsoft.CrmSdk.CoreAssemblies']")
+$existingWorkflow = $xml.SelectSingleNode("//PackageReference[@Include='Microsoft.CrmSdk.Workflow']")
+$existingNewtonsoftJson = $xml.SelectSingleNode("//PackageReference[@Include='Newtonsoft.Json']")
+
+# Check if SolutionDir already exists
+$existingSolutionDir = $xml.SelectSingleNode("//SolutionDir")
+
+if ($existingSolutionRef -and $existingTypeScriptRef -and $existingSolutionDir) {
     exit 0
 }
 
@@ -42,6 +49,51 @@ if (-not $existingTypeScriptRef) {
     $typeScriptRef.SetAttribute("Version", "2.1.*")
 
     $itemGroup.AppendChild($typeScriptRef) | Out-Null
+}
+
+if (-not $existingCoreAssemblies) {
+    $coreAssembliesRef = $xml.CreateElement("PackageReference", $xml.Project.NamespaceURI)
+    $coreAssembliesRef.SetAttribute("Include", "Microsoft.CrmSdk.CoreAssemblies")
+    $coreAssembliesRef.SetAttribute("Version", "9.0.*")
+
+    $itemGroup.AppendChild($coreAssembliesRef) | Out-Null
+}
+
+if (-not $existingWorkflow) {
+    $workflowRef = $xml.CreateElement("PackageReference", $xml.Project.NamespaceURI)
+    $workflowRef.SetAttribute("Include", "Microsoft.CrmSdk.Workflow")
+    $workflowRef.SetAttribute("Version", "9.0.*")
+    
+    $itemGroup.AppendChild($workflowRef) | Out-Null
+}
+
+if (-not $existingNewtonsoftJson) {
+    $newtonsoftJsonRef = $xml.CreateElement("PackageReference", $xml.Project.NamespaceURI)
+    $newtonsoftJsonRef.SetAttribute("Include", "Newtonsoft.Json")
+    $newtonsoftJsonRef.SetAttribute("Version", "13.0.*")
+    
+
+    $itemGroup.AppendChild($newtonsoftJsonRef) | Out-Null
+}
+
+# Add SolutionDir to the second PropertyGroup
+if (-not $existingSolutionDir) {
+    # Get all PropertyGroup elements
+    $propertyGroups = $xml.SelectNodes("//PropertyGroup")
+    
+    if ($propertyGroups.Count -ge 2) {
+        # Use the second PropertyGroup (index 1)
+        $secondPropertyGroup = $propertyGroups[1]
+    } else {
+        # If there's only one PropertyGroup, create a second one
+        $secondPropertyGroup = $xml.CreateElement("PropertyGroup", $xml.Project.NamespaceURI)
+        $xml.Project.AppendChild($secondPropertyGroup) | Out-Null
+    }
+    
+    # Create and add SolutionDir element
+    $solutionDirElement = $xml.CreateElement("SolutionDir", $xml.Project.NamespaceURI)
+    $solutionDirElement.InnerText = '$(MSBuildThisFileDirectory)..\..\'
+    $secondPropertyGroup.AppendChild($solutionDirElement) | Out-Null
 }
 
 $xml.Save($projectFile.FullName)
