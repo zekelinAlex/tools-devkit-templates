6cae334ce64ea5c7072de0811cfd0d4320db616b
Alexander Zekelin <aleksander.zekelin@networg.com>
2025-12-05 15:48:13 +0100

    sdk slyle set (temporarily)

--- src/Dataverse/templates/pp-package/.template.scripts/CreatePackage.ps1
+++ src/Dataverse/templates/pp-package/.template.scripts/CreatePackage.ps1
@@ -4,5 +4,13 @@ $csproj = "$name.csproj"
 pac package init --package-name $name 
 
 $content = Get-Content $csproj -Raw
-$content = $content -replace '<PackageReference Include="Microsoft\.PowerApps\.MSBuild\.PDPackage" Version="1.*">', '<PackageReference Include="TALXIS.DevKit.Build.Dataverse.PdPackage" Version="0.0.0.1">'
+$content = $content -replace '<PackageReference Include="Microsoft\.PowerApps\.MSBuild\.PDPackage" Version="1.*">', ''
+$content = $content -replace '      <PrivateAssets>all</PrivateAssets>', ''
+$content = $content -replace '      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>', ''
+$content = $content -replace '    </PackageReference>', ''
+$content = $content -replace '    </PackageReference>', ''
+
+
+$content = $content -replace '<Copyright>Copyright © 2025</Copyright>', '<Copyright>Copyright © 2025</Copyright>     <ProjectType>PdPackage</ProjectType>'
+
 Set-Content -Path $csproj -Value $content -NoNewline
