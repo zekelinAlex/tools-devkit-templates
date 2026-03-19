8c6e97ce73d8d3ce89d7693661ba9e6acaeca1f5
Alexander Zekelin <aleksander.zekelin@networg.com>
2026-03-19 16:04:27 +0100

    pp-pcf update

--- src/Dataverse/templates/pp-pcf/.template.scripts/CreatePCF.ps1
+++
@@ -1,7 +0,0 @@
-$name = "examplesourcename"
-$namespace = $name -replace '[^\p{L}\p{N}]', ''
-
-pac pcf init --namespace $namespace --name $namespace --template field
-
-Remove-Item -Path ".\$namespace.pcfproj" -Force
-Copy-Item -Path ".template.temp\examplesourcename.csproj" -Destination ".\$namespace.csproj"
